d5a4a01e9eeba6ac7e55d254104f09e986e843f8
Charley Reeson <neon.fortytwo@gmail.com>
2025-05-09 00:24:51 +0100

    initial magic

---
+++ src/viteExportEnumInterfaces.ts
@@ -0,0 +1,121 @@
+/* eslint-disable no-console */
+
+import { promises as fs } from "fs";
+import { exec } from "child_process";
+import chokidar, { FSWatcher, type ChokidarOptions } from "chokidar";
+import type { Plugin, ResolvedConfig } from "vite";
+
+interface PluginOptions {
+  enumDir: string;
+  enumEndpoint: string;
+  interfaceOutput: string;
+  // todo: better typing
+  chokidarOptions?: ChokidarOptions;
+  prettierExec?: string;
+}
+
+// https://decipher.dev/30-seconds-of-typescript/docs/debounce/
+function debounce(fn: Function, ms = 300) {
+  let timeoutId: ReturnType<typeof setTimeout>;
+  return function (this: any, ...args: any[]) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => fn.apply(this, args), ms);
+  };
+}
+
+const defaultOptions = {
+  enumDir: "",
+  enumEndpoint: "",
+  interfaceOutput: "",
+};
+
+const defaultChokidarOptions: ChokidarOptions = {
+  ignoreInitial: true,
+  atomic: false,
+  awaitWriteFinish: {
+    pollInterval: 100,
+  },
+  persistent: false,
+  interval: 300,
+};
+
+export function exportEnumInterface(options: PluginOptions): Plugin {
+  let resolvedConfig: ResolvedConfig = {} as ResolvedConfig;
+
+  const pluginConfig: Required<PluginOptions> = {
+    ...defaultOptions,
+    ...options,
+    chokidarOptions: {
+      ...defaultChokidarOptions,
+    },
+  };
+
+  Object.assign(pluginConfig.chokidarOptions, options.chokidarOptions ?? {});
+
+  const listenToEnumFolder = debounce(async function (e: string) {
+    if (e.startsWith(pluginConfig.enumDir.slice(2))) {
+      await exportEnums();
+    }
+  }, 200);
+
+  async function exportEnums() {
+    const get = async function () {
+      const response = await fetch(pluginConfig.enumEndpoint);
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch enums");
+      }
+
+      const json = await response.json();
+
+      await fs.writeFile(
+        pluginConfig.interfaceOutput,
+        `interface EnumInterface ${JSON.stringify(json)};\n`
+      );
+
+      // Make prettier happy.
+      exec(
+        `node_modules/.bin/prettier --write ${pluginConfig.interfaceOutput}`
+      );
+    };
+
+    console.info("Rebuilding enums file...");
+
+    try {
+      await get();
+      console.info("... Rebuilt enums file!");
+    } catch (e) {
+      console.error(
+        "Failed to rebuild enums file. Trying again in 2 seconds.",
+        e
+      );
+
+      setTimeout(exportEnums, 2000);
+    }
+  }
+
+  let fsWatcher: FSWatcher | null = null;
+
+  return {
+    name: "export-enum-interface",
+    configResolved(config) {
+      resolvedConfig = config;
+
+      fsWatcher = chokidar
+        .watch(pluginConfig.enumDir, pluginConfig.chokidarOptions)
+        .on("change", listenToEnumFolder)
+        .on("add", listenToEnumFolder)
+        .on("unlink", listenToEnumFolder);
+
+      if (resolvedConfig.mode === "development") {
+        exportEnums();
+      }
+    },
+
+    buildEnd() {
+      if (fsWatcher) {
+        fsWatcher.close();
+      }
+    },
+  };
+}
---
+++ vite.config.ts
@@ -0,0 +1,60 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig, UserConfig } from "vite";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const baseConfig: UserConfig = {
+    plugins: [
+      dts(
+        mode === "package"
+          ? {
+              entryRoot: "./src/vue-slick-press/",
+              tsconfigPath: "./tsconfig.app.json",
+            }
+          : {}
+      ),
+    ],
+    base: "/vue-slick-press/",
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  };
+
+  if (mode === "package") {
+    return {
+      ...baseConfig,
+      build: {
+        outDir: "./dist",
+        emptyOutDir: true,
+        lib: {
+          entry: resolve(__dirname, "src/vue-slick-press/index.ts"),
+          name: "VueSlickPress",
+          formats: ["es", "cjs"],
+        },
+      },
+      rollupOptions: {
+        external: ["vue"],
+        output: {
+          // Provide global variables to use in the UMD build
+          // for externalized deps
+          globals: {
+            vue: "Vue",
+          },
+        },
+      },
+    };
+  } else {
+    return {
+      ...baseConfig,
+      build: {
+        outDir: "./docs",
+        emptyOutDir: true,
+      },
+    };
+  }
+});
